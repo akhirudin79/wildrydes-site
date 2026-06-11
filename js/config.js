45a4b6f68ab7d39adfb4b94688cfe3ddbcd63a43
akhirudin79 <akhirudin79@yahoo.com>
2026-06-11 20:37:42 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'ap-southeast-2' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };

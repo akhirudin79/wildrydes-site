97717d2c96c16ed7a3e5c8e098c9da77852b2136
akhirudin79 <akhirudin79@yahoo.com>
2026-06-11 20:46:38 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-2_zDbChLhPp', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '701bfmmggilgg0dqss10ldqhcg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '50as4jh9flt4pas266138rvehf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
